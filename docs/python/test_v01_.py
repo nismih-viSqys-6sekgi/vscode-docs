37e4b9aa103e71cd2e472a62bb7690523d73fb01
nismih-viSqys-6sekgi <150675608+nismih-viSqys-6sekgi@users.noreply.github.com>
2025-08-12 23:54:18 +0900

    test_v01_.py を更新

--- docs/python/test_v01_.py
+++ docs/python/test_v01_.py
@@ -3,3 +3,32 @@ b = 3
 c = a + b
 print(c)
 
+# Wmat電力 休日穴埋め
+
+f_n = 4567
+b_n = 5678
+hol_n = 12
+
+sp = 15
+
+
+
+print()
+print(b_n - f_n)
+
+def cal(b_n, f_n, hol_n):
+	res = int((b_n - f_n) / (hol_n + 1))
+	print(res)
+	print()
+	
+	for i in range(hol_n):
+		f_n += res
+		print(f_n)
+		
+		if i == sp:
+			print()
+			print(f_n)
+		
+	
+cal(b_n, f_n, hol_n)
+
