ad1310f961d9e88255eb11523060eff0d40e9548
nismih-viSqys-6sekgi <150675608+nismih-viSqys-6sekgi@users.noreply.github.com>
2025-11-06 04:59:13 +0900

    test_v01_.py を更新

--- docs/python/test_v01_.py
+++ docs/python/test_v01_.py
@@ -1,1 +1,16 @@
-import fleet as ft
+import flet as ft
+
+def main(page: ft.Page):
+    # アプリケーションのタイトル設定
+    page.title = "はじめてのFletアプリ"
+    
+    # UIコンポーネントの追加
+    page.add(
+        ft.Text("こんにちは、Flet!",
+                size=30,
+                color="blue",
+                weight="bold")
+    )
+
+# アプリケーションの実行
+ft.app(target=main)
