e80ed829663e7d3defa6972f719bbfd237453f8b
nismih-viSqys-6sekgi <150675608+nismih-viSqys-6sekgi@users.noreply.github.com>
2025-11-29 14:59:21 +0900

    test_v01_.py を更新

--- docs/python/test_v01_.py
+++ docs/python/test_v01_.py
@@ -1,16 +1,29 @@
 import flet as ft
 
 def main(page: ft.Page):
-    # アプリケーションのタイトル設定
-    page.title = "はじめてのFletアプリ"
-    
-    # UIコンポーネントの追加
+    page.title = "Flet counter example"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
     page.add(
-        ft.Text("こんにちは、Flet!",
-                size=30,
-                color="blue",
-                weight="bold")
+        ft.Row(
+            [
+                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
     )
 
-# アプリケーションの実行
-ft.app(target=main)
+ft.app(main)
+
