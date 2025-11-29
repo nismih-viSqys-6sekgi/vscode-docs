acf45b2a299d56204ca4d9e7c9bc9a7483811b57
nismih-viSqys-6sekgi <150675608+nismih-viSqys-6sekgi@users.noreply.github.com>
2025-11-29 15:24:56 +0900

    test_v01_.py を更新

--- docs/python/test_v01_.py
+++ docs/python/test_v01_.py
@@ -1,29 +1,60 @@
+
+
+
+
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "Flet counter example"
+    page.title = "Flet Two Counters Example"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    # カウンター1の部品とロジック
+    txt_number1 = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
 
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
+    def minus_click1(e):
+        txt_number1.value = str(int(txt_number1.value) - 1)
         page.update()
 
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
+    def plus_click1(e):
+        txt_number1.value = str(int(txt_number1.value) + 1)
         page.update()
 
+    # カウンター2の部品とロジック
+    txt_number2 = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click2(e):
+        txt_number2.value = str(int(txt_number2.value) - 1)
+        page.update()
+
+    def plus_click2(e):
+        txt_number2.value = str(int(txt_number2.value) + 1)
+        page.update()
+
+    # カウンター1とカウンター2のUIを追加
     page.add(
-        ft.Row(
+        ft.Column(
             [
-                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
+                ft.Text("カウンター1"),
+                ft.Row(
+                    [
+                        ft.IconButton(ft.Icons.REMOVE, on_click=minus_click1),
+                        txt_number1,
+                        ft.IconButton(ft.Icons.ADD, on_click=plus_click1),
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                ),
+                ft.Text("カウンター2"),
+                ft.Row(
+                    [
+                        ft.IconButton(ft.Icons.REMOVE, on_click=minus_click2),
+                        txt_number2,
+                        ft.IconButton(ft.Icons.ADD, on_click=plus_click2),
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                ),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         )
     )
 
 ft.app(main)
-
